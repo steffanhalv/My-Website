1559b5507ddd158d5cdffd07ba8b71dbd0e5b727
Vue Play <author@vueplay.com>
2023-09-28 10:55:37 +0200

    Stored by Vue Play

---
+++ src/vueplay/PublicComponent.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="p-6 bg-slate-200">
+		<h2
+			class="mb-12"
+			style="font-weight:147"
+		>
+			This is a Public Component Hello World!
+		</h2><input
+			class="mb-6 shadow rounded p-2 w-full"
+			placeholder="Type something"
+			v-model="message"
+		/><span style="font-weight:583">
+			{{message}}
+		</span><span
+			class="block"
+			style="color:#d92525ff"
+		>
+			State is preserved if only the template is updated.
+		</span>
+	</div>
+</template>
+<script>
+	export default {
+		data: () => ({
+			message: ''
+		})
+	};
+
+</script>
+<style></style>
